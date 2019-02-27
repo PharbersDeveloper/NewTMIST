299aef3daf20679c174995d55bcbfef721696f12
clock <2567729201@qq.com>
2019-02-27 17:35:07 +0800

    tmp

--- main.go
+++ main.go
@@ -2,37 +2,33 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/alfredyang1986/BmServiceDef/BmApiResolver"
 	"github.com/alfredyang1986/BmServiceDef/BmConfig"
 	"github.com/alfredyang1986/BmServiceDef/BmPodsDefine"
-	"github.com/julienschmidt/httprouter"
-	"github.com/manyminds/api2go"
 	"os"
 )
 
 func main() {
-	version := "v2"
-	fmt.Println("pod archi begins")
-	var pod = BmPodsDefine.Pod{Name: "alfred test"}
-	bmHome := os.Getenv("BM_HOME")
-	pod.RegisterSerFromYAML(bmHome + "/resource/service-def.yaml")
+	version := "v0"
+	fmt.Println("NTM pods archi begins, version =", version)
+
+	var pod = BmPodsDefine.Pod{Name: "new TMIST"}
+	ntmHome := os.Getenv("NTM_HOME")
+	pod.RegisterSerFromYAML(ntmHome + "/Resources/service-def.yaml")
 
 	var bmRouter BmConfig.BmRouterConfig
 	bmRouter.GenerateConfig()
 
 	addr := bmRouter.Host + ":" + bmRouter.Port
 	fmt.Println("Listening on ", addr)
-	api := api2go.NewAPIWithResolver(version, &BmApiResolver.RequestURL{Addr: addr})
-	pod.RegisterAllResource(api)
+	//api := api2go.NewAPIWithResolver(version, &BmApiResolver.RequestURL{Addr: addr})
+	//pod.RegisterAllResource(api)
 
-	pod.RegisterAllFunctions(version, api)
-	pod.RegisterAllMiddleware(api)
-
-	handler := api.Handler().(*httprouter.Router)
-	pod.RegisterPanicHandler(handler)
-	http.ListenAndServe(":"+bmRouter.Port, handler)
-
-	fmt.Println("pod archi ends")
+	//pod.RegisterAllFunctions(version, api)
+	//pod.RegisterAllMiddleware(api)
+	//
+	//handler := api.Handler().(*httprouter.Router)
+	//pod.RegisterPanicHandler(handler)
+	//http.ListenAndServe(":"+bmRouter.Port, handler)
+	//
+	fmt.Println("NTM pods archi ends, version =", version)
 }
